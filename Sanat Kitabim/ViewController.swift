1dfef189a0bfeafd40b21a405a26c1e40b4504ec
srhtsimsek <srhtsimsekk@gmail.com>
2023-06-05 05:31:11 +0300

    Initial Commit

---
+++ Sanat Kitabim/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sanat Kitabim
+//
+//  Created by Serhat  Şimşek  on 5.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
